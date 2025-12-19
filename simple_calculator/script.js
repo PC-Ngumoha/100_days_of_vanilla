287a5b0f7f81fcfb75dfb366e5a5fee1ed4d4109
PC-Ngumoha <pcngumoha@gmail.com>
2025-12-19 13:51:45 +0100

    Day 5: Add token to screen. Clear screen

--- simple_calculator/script.js
+++ simple_calculator/script.js
@@ -1,1 +1,23 @@
-console.log("JS script connected successfully !!!");
+/**
+ * script.js
+ */
+const screen = document.getElementById("screen");
+
+// Convert to array for easy manipulation
+const tokenBtns = [...document.getElementsByClassName("tokens")];
+
+tokenBtns.forEach((tokenBtn) => {
+  tokenBtn.addEventListener('click', () => {
+    addToScreen(tokenBtn.value);
+  });
+});
+
+
+// UTILITY FXN: Adds token to screen
+function addToScreen(token) {
+  screen.textContent += token;
+}
+
+function clearScreen() {
+  screen.textContent = '';
+}
